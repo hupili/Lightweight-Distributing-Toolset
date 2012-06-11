f699336278730b3e06f151c3394fd72fcbc6635c
hpl <hupili@ie.cuhk.edu.hk>
2012-06-11 18:10:46 +0800

    upgrade put-all-diff.pl to multiprocess version

--- put-all-diff.pl
+++ put-all-diff.pl
@@ -6,15 +6,29 @@ use Data::Dumper ;
 
 my $ARGC = @ARGV ;
 ($ARGC == 2) or die("$0 {local} {remote}\n") ;
-
 my ($local, $remote) = @ARGV ;
-my $ret = 0 ;
+
+`mkdir -p $tmp/result` ;
+
+my $multi_cmd = "" ;
 for (@a_host){
 	my $hostname = $_->{"hostname"} ;
 	my $home = $_->{"home"} ;
 	my $subdir = $hostname ;
-	my $r = system qq( scp -r $local/$subdir/* $hostname:$home/$remote ) ;
-	$ret |= $r ;
+	$multi_cmd .= qq(./put.pl $hostname $local/$subdir/* $remote ; echo \$? > $tmp/result/$hostname\n) ;
 }
 
-exit $ret ;
+#print $multi_cmd ;
+open f_cmd, ">$tmp/multi_cmd" ;
+print f_cmd $multi_cmd ;
+close f_cmd ;
+system qq( cat $tmp/multi_cmd | ./multiprocess.pl $multi_scp_count $multi_scp_timeout &> $tmp/multi_log) ;
+
+for my $f(`cd $tmp/result ; ls -1 .`){
+	chomp($f) ;
+	my $re = `cat $tmp/result/$f` ;
+	chomp($re) ;
+	print "$f:$re\n" ;
+}
+
+exit 0 ;
