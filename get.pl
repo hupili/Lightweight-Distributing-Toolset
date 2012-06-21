8cfc547e794533cc3fe1b27e436a7780a8a0cb77
hpl <hupili@ie.cuhk.edu.hk>
2012-06-21 22:03:36 +0800

    add fault tolerant to get/put: no such machine error

--- get.pl
+++ get.pl
@@ -8,6 +8,10 @@ my $ARGC = @ARGV ;
 ($ARGC == 3) or die("$0 {target} {remote} {local}\n") ;
 
 my ($target, $remote, $local) = @ARGV ;
+if ( ! exists $h_host{$target} ){
+	print STDERR "$target: host does not exists\n" ;
+	return -1 ;
+}
 my $home = $h_host{$target}->{"home"} ;
 my $ret = system qq( scp -C -r "$target:$home/$remote" $local ) ;
 
--- put.pl
+++ put.pl
@@ -8,6 +8,10 @@ my $ARGC = @ARGV ;
 ($ARGC == 3) or die("$0 {target} {local} {remote}\n") ;
 
 my ($target, $local, $remote) = @ARGV ;
+if ( ! exists $h_host{$target} ){
+	print STDERR "$target: host does not exists\n" ;
+	return -1 ;
+}
 my $home = $h_host{$target}->{"home"} ;
 my $ret = system qq( scp -C -r $local "$target:$home/$remote" ) ;
 
